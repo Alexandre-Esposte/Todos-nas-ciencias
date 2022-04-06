233751aa4c19751a61dd4108cd64ead0c5cf7e88
Alexandre-Esposte <alexandreesposte@gmail.com>
2022-04-06 15:23:19 -0300

    Grafico ok

--- interface2.py
+++ interface2.py
@@ -5,14 +5,30 @@ from turtle import width
 import tk_tools
 import serial
 import time
+import matplotlib.pyplot as plt
 
 ser=0 # variavel global
+volume_graf = []
+pressao_graf = []
+
+def plotar():
+    plt.title("Grafico pressão versus volume")
+    plt.scatter(volume_graf,pressao_graf)  
+    plt.xlabel("Volume (mL)")
+    plt.ylabel("Pressao (HPA)")
+    plt.savefig("grafico.png")
+    img = tk.PhotoImage(file="/home/uff/Área de Trabalho/todos na ciencia/grafico.png") 
+    grafico.configure(image=img)
+    grafico.imagem = img
+  
+    return
 
 def medir():
-    
+    global volume_graf,pressao_graf
     time.sleep(1.8)
     ser.write('m'.encode('utf-8'))
-    vol_var.set(f"Volume (mL): {volume_entry.get()}")
+    volume = float(volume_entry.get().replace(',','.'))+45.3
+    vol_var.set(f"Volume (mL): {volume}")
     time.sleep(1.8)
     temperatura = ser.readline()
     temperatura = temperatura.decode('utf-8')
@@ -21,9 +37,13 @@ def medir():
     pressao = ser.readline()
     pressao = pressao.decode('utf-8')
     pressao_var.set(f"Pressão (HPa): {pressao}")
+    
+    volume_graf.append(volume)
+    pressao_graf.append(float(pressao))
 
-
+    plotar()
     return
+
 def desconectar():
     global ser
 
@@ -57,7 +77,7 @@ def conectar():
     return
 
 
-largura = 800
+largura = 1000
 altura = 600
 janela= tk.Tk()
 
@@ -68,8 +88,8 @@ frame1 = tk.Frame(janela,height=35)
 frame2 = tk.Frame(janela)
 frame3 = tk.Frame(janela)
 frame1.place(relx=0,rely=0,relwidth=1)
-frame2.place(relx=0,y=50,relwidth=0.33,relheight=1)
-frame3.place(relx =0.33,relheight=1,y=50,relwidth=1)
+frame2.place(relx=0,y=50,relwidth=0.2,relheight=1)
+frame3.place(relx =0.2,relheight=1,y=50,relwidth=1)
 
 #--------- FRAME 1 ---------------
 '''Este frame conterá os widgets e as funções para a conexão com o arduino'''
@@ -91,14 +111,14 @@ ard_label.place(relx=0.05)
 ttk.Separator(frame1, orient='horizontal').place(x=0,rely=0.95,relwidth=1)
 
 #--------FRAME 2------------
-ttk.Separator(frame2, orient='vertical').place(relheight=1,relx=0.998)
-volume_txt = tk.Label(frame2,text="Informe o volume na seringa")
+ttk.Separator(frame2, orient='vertical').place(relheight=1,relx=0.9974)
+volume_txt = tk.Label(frame2,text="Volume indicado")
 volume_entry_var = tk.StringVar()
 volume_entry = tk.Entry(frame2,width=10,textvariable=volume_entry_var)
 volume_txt.pack()
 volume_entry.pack()
 
-massa_txt = tk.Label(frame2,text='Massa utilizada no embolo da seringa')
+massa_txt = tk.Label(frame2,text='Massa no embolo')
 massa_entry_var= tk.StringVar()
 massa_entry = tk.Entry(frame2,width=10,textvariable=massa_entry_var)
 massa_txt.pack(pady=10)
@@ -125,7 +145,10 @@ vol_txt = tk.Label(frame2,textvariable=vol_var,pady=30)
 vol_txt.pack()
 
 btn_medir = tk.Button(frame2,text='Medir',command=medir)
-btn_medir.place(rely=0.8,relheight=0.1,relwidth=1)
+btn_medir.place(relx=0.05,rely=0.8,relheight=0.1,relwidth=0.9)
 
+# ----------- Frame 3 ---------------------
+grafico = tk.Label(frame3)
+grafico.place(relx=0,rely=0)
 
 janela.mainloop()
